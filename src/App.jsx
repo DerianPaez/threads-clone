7d99435a2d1af71bc60f21e41dad668f0b3a5896
DerianPaez <derianpaez01@hotmail.com>
2023-08-31 19:39:08 -0500

    integrate backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import "./App.css";
 import { Button } from "@nextui-org/react";
 
 function App() {
+  async function handlePing() {
+    try {
+      const response = await fetch(
+        "https://threads-clone-restapi-dev.up.railway.app/ping"
+      );
+      const data = await response.text();
+      alert(data);
+    } catch (error) {
+      console.error("Error en la solicitud al backend:", error);
+    }
+  }
+
   return (
     <>
-      <Button onClick={() => alert("Pong")}>Ping</Button>
+      <Button onClick={handlePing}>Ping</Button>
     </>
   );
 }
